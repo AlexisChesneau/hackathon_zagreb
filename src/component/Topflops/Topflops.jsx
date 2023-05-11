25bdd14c65bd97db2aae65b51cc9eb44ac67e5c2
Pierre Lacoste <p.lacoste33000@gmail.com>
2023-05-12 01:16:40 +0200

    2eme partie squelette

--- src/component/Topflops/Topflops.jsx
+++ src/component/Topflops/Topflops.jsx
@@ -2,41 +2,76 @@ import './Topflops.css';
 
 export default function TopFlops() {
   return (
-    <div className='topflop'>
-      <h1>Les Tops</h1>
+    <div className="topflop">
+      <div className="top">
+        <h1>Les Tops</h1>
+      </div>
+      <div className="topville">
+        <div className="container1">
+          <div className="image-bjornoya">
+            <h2>Bjornoya</h2>
+            <img
+              src="../src/assets/images/Bjornoya.jpg"
+              alt="Photo de Bjornoya"
+            ></img>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
 
-      <div className="container1">
-        <h2>Bjornoya</h2>
-        <img
-          src="../src/assets/images/Bjornoya.jpg"
-          alt="Photo de Bjornoya"
-        ></img>
+        <div className="container2">
+          <div className="image-remy">
+            <h2>Saint-Remy-en-Bouzemont-Saint-Genest-et-Isson</h2>
+            <img
+              src="../src/assets/images/Saint-Remy-en-Bouzemont-Saint-Genest-et-Isson.jpeg"
+              alt="Photo de Saint-Remy-en-Bouzemont-Saint-Genest-et-Isson"
+            ></img>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
       </div>
 
-      <div className="container2">
-        <h2>Saint-Remy-en-Bouzemont-Saint-Genest-et-Isson</h2>
-      <img
-          src="../src/assets/images/Saint-Remy-en-Bouzemont-Saint-Genest-et-Isson.jpeg"
-          alt="Photo de Saint-Remy-en-Bouzemont-Saint-Genest-et-Isson"
-        ></img>
+      <div className="flop">
+        <h1>Les Flops</h1>
       </div>
-      
-      <h1>Les Flops</h1>
+      <div className="flopville">
+        <div className="container3">
+          <div className="image-hum">
+            <h2>Hum</h2>
+            <img src="../src/assets/images/Hum.jpeg" alt="Photo de Hum"></img>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
 
-      <div className="container3">
-        <h2>Hum</h2>
-        <img
-          src="../src/assets/images/Hum.jpeg"
-          alt="Photo de Hum"
-        ></img>
-      </div>
-      
-      <div className="container4">
-        <h2>Paris</h2>
-      <img
-          src="../src/assets/images/Paris.webp"
-          alt="Photo de Paris"
-        ></img>
+        <div className="container4">
+          <div className="image-paris">
+            <h2>Paris</h2>
+            <img
+              src="../src/assets/images/Paris.png"
+              alt="Photo de Paris"
+            ></img>
+          </div>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </div>
       </div>
     </div>
   );
