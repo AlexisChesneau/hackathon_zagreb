eff7e92d44124e29d0256c114e35cf98377bc863
Pierre Lacoste <p.lacoste33000@gmail.com>
2023-05-11 21:25:58 +0200

    premiere partie

--- src/component/Footer/Footer.jsx
+++ src/component/Footer/Footer.jsx
@@ -1,10 +1,23 @@
-import "./Footer.css";
+import './Footer.css';
 
 export default function Footer() {
   return (
     <>
       <footer>
-        <p></p>
+        <img
+          className="charo"
+          src="./src/assets/icons/copyright.svg"
+          alt="copyright"
+        />
+        <div className="pouloulou">
+          <div className="reseau">
+            <img src="./src/assets/icons/facebook.svg" alt="A" />
+
+            <img src="./src/assets/icons/instagram.svg" alt="B" />
+
+            <img src="./src/assets/icons/twitter.svg" alt="C" />
+          </div>
+        </div>
       </footer>
     </>
   );
